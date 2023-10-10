6b3f031a8594ff290ee3a0e15279656793b14420
Arthur Rogado <arthurrogadoreis@hotmail.com>
2023-10-09 23:18:42 -0300

    updates

--- App/Utils/constants.py
+++ App/Utils/constants.py
@@ -6,6 +6,6 @@ ADMIN_ID = ''
 
 # Web URLs
 
-URL_WEBAPP = "https://noble-capital-wasp.ngrok-free.app#" # changes to your webapp url if you want to
+URL_WEBAPP = "https://miniapp-simple-image-generator.netlify.app" # changes to your webapp url if you want to
 # Default routes in webapp
 URL_HOME = URL_WEBAPP + "home"
